482e920e98d7dc76b4a29dc8432a4cf0ce255e4e
wuz1mu <kingaal93@gmail.com>
2023-02-04 14:22:16 +0700

    Install additional dependencies. Add config file

---
+++ backend/src/config/config.js
@@ -0,0 +1,16 @@
+import path from "path";
+
+export default {
+    defaultPort: 8081,
+    db: {
+        database: process.env.DB_NAME || 'image_host',
+        user: process.env.DB_USER || 'postgres',
+        password: process.env.DB_PASSWORD || 'postgres',
+        options: {
+            dialect: process.env.DB_DIALECT || 'postgres',
+            host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT || 5432
+        }
+    },
+    uploadsDir: path.resolve(path.resolve(), 'uploads')
+}
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,24 @@
 import express from "express";
 import path from "path";
+import config from "./config/config.js";
 
 const __dirname = path.resolve();
-const PORT = process.env.PORT || 9999;
+const PORT = process.env.PORT || config.defaultPort;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(express.static(path.resolve(__dirname, 'uploads')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve("static", "index.html"));
-});
-
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT} port`)
 });
+
+// sequelize.sync()
+// .then(() => {
+//     app.listen(PORT, () => {
+//         console.log(`Server started on ${PORT} port`)
+//     });
+// });
+
+
+
