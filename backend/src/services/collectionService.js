40dc6d76e83e09d00bf4ced8dbbdd1b4949ce943
wuz1mu <kingaal93@gmail.com>
2023-02-07 21:06:43 +0700

    Implement file and collection services

--- backend/src/services/collectionService.js
+++ backend/src/services/collectionService.js
@@ -1,22 +1,43 @@
 import { Collection } from "../models/index.js"
+import fileService from './fileService.js'
+import path from "path";
+import config from "../config/config.js";
+import log4js from 'log4js';
+
+const logger = log4js.getLogger('CollectionService');
+logger.level = 'debug';
 
 export default {
     async createCollection(collectionData) {
         const collectionModel = Collection.build(collectionData);
         try{
             await collectionModel.save();
-            console.log('Collection saved: ', collectionModel.name);
+            await fileService.createDirectory(path.resolve(config.uploadsDir, collectionModel.name));
+            logger.info('Collection saved: ', collectionModel.name);
         }
         catch(err) {
-            console.log('Error when creating collection ', err)
+            logger.error('Error when creating collection ', err)
         }
-
-        //createDirectory();
     },
-    updateCollection(colId, colFields) {
-        
+    async deleteCollection(colId) {
+        const collectionModel = await Collection.findOne({
+            where: {
+                id: colId
+             }
+        });
+        if(!collectionModel) {
+            throw new Error(`Collection with id ${colId} does not exist`)
+        }
+        await fileService.deleteDirectory(path.resolve(config.uploadsDir, collectionModel.name));
+        await collectionModel.destroy();
+        logger.info(`Collection with id ${colId} removed.`);
     },
-    deleteCollection(colId) {
-        
+    async collectionExistsById(colId) {
+        const collectionModel = await Collection.findOne({
+            where: {
+                id: colId
+             }
+        });
+        return collectionModel !== null;
     }
 }
--- backend/src/services/fileService.js
+++ backend/src/services/fileService.js
@@ -1,33 +1,69 @@
 import path from 'path';
 import fs from 'fs';
-import config from '../config/config.js';
 import log4js from 'log4js';
 
-const logger = log4js.getLogger();
+const logger = log4js.getLogger('FileService');
 logger.level = 'trace';
 
-const service = {
-    async saveFile (content, path, props = {}) {
-        await fs.writeFile(path, content, (err) => {
-            if(err) {
-                logger.error('Error creating dir', err)
-                throw new Error('Error when saving file. ' + err.message);
-            }
+const fileService = {
+    async saveFile(content, path, props = {}) {
+        return new Promise((res, rej) => {
+            fs.writeFile(path, content, (err) => {
+                if (err) {
+                    logger.error('Error creating dir', err);
+                    rej(new Error('Error when saving file. ' + err.message));
+                }
+                else {
+                    res('File saved');
+                }
+            });
         });
+
     },
-    async createDirectory(dir) {
-        if(!await fs.existsSync(dir)) {
-            await fs.mkdir(dir, {recursive: false}, (err) => {
-                if(err) {
+    async createDirectory(dir, props = { recursive: true }) {
+        if (!await this.dirExists(dir)) {
+            await fs.mkdir(dir, props, (err) => {
+                if (err) {
                     logger.error('Error creating dir', err)
-                    throw new Error('Error creating dir. ' + err.message);
+                    throw new Error(`Error creating directory ${dir}. ` + err.message);
                 }
             });
         }
-        else{
+        else {
             logger.info('Directory already exists');
         }
+    },
+    async deleteDirectory(dir, props = { recursive: true }) {
+        logger.debug(`Trying to delete directory: ${dir}`);
+        if (await this.dirExists(dir)) {
+            await fs.rmdir(dir, props, (err) => {
+                if (err) {
+                    logger.error('Error creating dir', err);
+                    throw new Error(`Error deliting directory ${dir}. ` + err.message);
+                }
+                else {
+                    logger.info(`Directory removed ${dir}`, err);
+                }
+            })
+        }
+        else {
+            logger.info('Directory does not exist');
+        }
+    },
+    async dirExists(dir) {
+        return new Promise((res, rej) => {
+            fs.access(dir, (err) => {
+                if (err) {
+                    logger.warn(`Error checking directory: ${dir}. `, err)
+                    res(false);
+                }
+                else {
+                    logger.info(`Directory exists: ${dir}. `)
+                    res(true);
+                }
+            });
+        })
     }
 };
 
-export default service;
+export default fileService;
